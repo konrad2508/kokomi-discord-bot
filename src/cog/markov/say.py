c8f6f4961f7a72f336fe545e9db1ffb9159d5a08
Konrad Grela <konrad1997@gmail.com>
2023-01-27 19:51:25 +0100

    Add learn and say markov commands

---
+++ src/cog/markov/say.py
@@ -0,0 +1,71 @@
+from functools import wraps
+from typing import Callable, Type, Union
+
+from nextcord import TextChannel
+from nextcord.abc import GuildChannel
+# from nextcord.abc import GuildChannel
+from nextcord.ext import commands
+
+from messages import Messages
+from model.exception.missing_argument import MissingArgument
+from model.exception.not_in_server import NotInServer
+from service.api_wrapper_service import APIWrapperService
+from service.embed_sender_service import EmbedSenderService, embed_sender_service
+from service.markov_service import MarkovService, markov_service
+
+
+class SayCog(commands.Cog):
+    '''Class representing the learn command. This starts the learning process on the specified channel,
+    or updates the model on new messages.'''
+
+    def __init__(
+            self,
+            aw: Type[APIWrapperService],
+            ess: EmbedSenderService,
+            ms: MarkovService) -> None:
+        self.api_wrapper = aw
+        self.embed_sender_service = ess
+        self.markov_service = ms
+
+    @staticmethod
+    def checker(func: Callable) -> Callable:
+        '''Decorator checking whether the learn command can be run.
+        
+        The command can be run if invoked in the server, and the user has privileges to do so.'''
+
+        @wraps(func)
+        async def decorator(self: 'SayCog', ctx: commands.Context, *, channel: Union[TextChannel, str]):
+            api = self.api_wrapper(ctx)
+
+            try:
+                api.check_if_author_in_server()
+
+                if channel is ...:
+                    raise MissingArgument
+                
+                if type(channel) is not TextChannel:
+                    raise Exception
+
+                await func(self, ctx, channel=channel)
+
+            except NotInServer:
+                await self.embed_sender_service.send_error(ctx, Messages.AUTHOR_NOT_IN_SERVER)
+
+            except MissingArgument:
+                await self.embed_sender_service.send_error(ctx, Messages.MISSING_ARGUMENTS)
+
+        return decorator
+
+    @commands.command(name='say')
+    @checker
+    async def say_command(self, ctx: commands.Context, *, channel: Union[TextChannel, str] = ...) -> None:
+        '''Body of the command.'''
+
+        start, msg = await self.markov_service.say(channel)
+
+        await self.embed_sender_service.send_success(ctx, start)
+        await self.embed_sender_service.send_success(ctx, msg)
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(SayCog(APIWrapperService, embed_sender_service, markov_service))
---
+++ src/service/markov_service.py
@@ -0,0 +1,101 @@
+import random
+import re
+from nextcord import TextChannel, Object
+
+from model.exception.channel_not_learned import ChannelNotLearned
+from model.markov.markov_grammar import MarkovGrammar
+
+
+class MarkovService:
+    '''Class responsible for operating the Markov Chain model.'''
+    def __init__(self) -> None:
+        self.grammars: dict[int, MarkovGrammar] = {}
+        self.sentence_starts: dict[int, list[str]] = {}
+        self.gram_n = 3
+        self.max_message_length = 500
+
+    async def learn(self, channel: TextChannel) -> bool:
+        '''Learns the model for a specified channel. Returns bool value indicating success or failure.'''
+
+        # messages = [msg async for msg in channel.history(oldest_first=False, after=Object(1067950472264175636))]
+        messages = [msg async for msg in channel.history(oldest_first=False)]
+
+        messages_content = [msg.content for msg in messages]
+
+        # print(messages_content)
+        # print([msg.id for msg in messages])
+
+        # 1. filter messages - command invokations, hyperlinks, mentions etc
+        # 1.1 remove hyperlinks
+        messages_content = [ re.sub(r'(?:https?|ftp)\:\/\/.*?(?:$|\s)', '', msg)    for msg in messages_content ]
+
+        # 1.2 remove command invokations, mentions, tags etc
+        messages_content = [ re.sub(r'(?:^|\s)[!@#$%^&*\-\+=\?\/\.\,][^\s]+', '', msg) for msg in messages_content ]
+        
+        # 2. tokenize messages
+        tokenized_messages = [ [ m for m in msg.split(' ') if m != '' ] for msg in messages_content ]
+        
+        # 2.1. generate n-grams
+        ngrams = [ zip(*[ tokenized[i:] for i in range(self.gram_n) ]) for tokenized in tokenized_messages ]
+        ngrams = [ [' '.join(ng) for ng in ngram] for ngram in ngrams ]
+        ngrams = [ ngram for ngram in ngrams if len(ngram) != 0 ]
+        
+        # 3. create and save grammar
+        if channel.id not in self.grammars:
+            self.grammars[channel.id] = MarkovGrammar()
+            self.sentence_starts[channel.id] = []
+        
+        for n in ngrams:
+            for m in n:
+                splitted = m.split(' ')
+
+                input = ' '.join(splitted[:-1])
+                
+                # normalize input
+                input = self._normalize_input(input)
+
+                output = m
+
+                self.grammars[channel.id] += [input, output]
+
+        # 4. save sentence start
+        for n in ngrams:
+            splitted_first_ngram = n[0].split(' ')
+
+            self.sentence_starts[channel.id].append(' '.join(splitted_first_ngram[:-1]))
+        
+        # 5. verify
+        # self.grammars[channel.id].print_matrixes()
+        # print(self.sentence_starts[channel.id])
+    
+    async def say(self, channel: TextChannel) -> str:
+        if channel.id not in self.grammars:
+            raise ChannelNotLearned
+
+        # choose a starting ngram
+        starting = random.choice(self.sentence_starts[channel.id])
+
+        # generate a poem
+        generated_message = starting
+
+        try:
+            while len(generated_message) < self.max_message_length:
+                input = ' '.join(generated_message.split(' ')[1 - self.gram_n:])
+                
+                # normalize input
+                input = self._normalize_input(input)
+                
+                output = self.grammars[channel.id][input]
+
+                generated_message = f'{" ".join(generated_message.split(" ")[:1 - self.gram_n])} {output}'
+        
+        finally:
+            return (starting, generated_message)
+    
+    def _normalize_input(self, input: str) -> str:
+        input = re.sub(r'[^a-zA-Z0-9\s]', ' ', input)
+
+        return input
+
+
+markov_service = MarkovService()
