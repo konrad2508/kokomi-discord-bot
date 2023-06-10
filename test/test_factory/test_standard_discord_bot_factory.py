92cfe6e60bc2156cb87c233f12170969518ab32d
Konrad Grela <konrad1997@gmail.com>
2023-06-10 21:55:54 +0200

    Check for Intents object

--- test/test_factory/test_standard_discord_bot_factory.py
+++ test/test_factory/test_standard_discord_bot_factory.py
@@ -8,6 +8,8 @@ class StandardDiscordBotFactoryUnitTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.bot_ctor_mock = patch('factory.standard_discord_bot_factory.commands.Bot').start()
         self.bot_obj_mock = self.bot_ctor_mock.return_value
+        self.intents = patch('factory.standard_discord_bot_factory.Intents').start()
+        self.intents_default_obj = self.intents.default.return_value
 
         self.cfg = MagicMock()
         self.obj = StandardDiscordBotFactory(self.cfg)
@@ -23,13 +25,11 @@ class StandardDiscordBotFactoryUnitTestCase(unittest.TestCase):
     def test_get_bot_returned_instance_received_correct_ctor_params(self) -> None:
         self.obj.get_bot()
 
-        self.bot_ctor_mock.assert_called_once_with(command_prefix=self.cfg.prefix, help_command=None)
+        self.bot_ctor_mock.assert_called_once_with(command_prefix=self.cfg.prefix, help_command=None, intents=self.intents_default_obj)
+        self.assertEqual(self.intents_default_obj.message_content, True)
     
     def test_get_bot_loads_extensions_to_bot(self) -> None:
         self.obj.get_bot()
 
         self.bot_obj_mock.load_extension.assert_called()
 
-        calls = self.bot_obj_mock.load_extension.call_args_list
-        for call in calls:
-            print(call)
