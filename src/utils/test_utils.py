6147d035c4e1fc382b6a3b9c0bd029d6d023cbc5
Konrad Grela <konrad1997@gmail.com>
2023-06-20 21:12:39 +0200

    Fix missing envs in SpotipyPCMSourceIntegrationTestCase

--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -3,21 +3,15 @@ from typing import Callable
 from unittest.mock import AsyncMock, MagicMock, patch, call
 
 
-def tested_module(source_module: str) -> Callable:
-    def inner(test_case: TestCase) -> TestCase:
-        test_case._source = source_module
-
-        return test_case
-    
-    return inner
-
-
 class TestCase(unittest.IsolatedAsyncioTestCase):
     def tearDown(self) -> None:
         patch.stopall()
     
     def patch(self, target: str):
         return patch(f'{self._source}.{target}').start()
+    
+    def patch_dict(self, target: dict, substitute: dict) -> None:
+        patch.dict(target, substitute).start()
     
     def assertNotRaises(self, expected_exception: Exception, callable: Callable, *args, **kwargs) -> None:
         try:
@@ -27,3 +21,12 @@ class TestCase(unittest.IsolatedAsyncioTestCase):
     
     def assertHasCalls(self, mock: MagicMock | AsyncMock, call_args: list[tuple[object]]) -> None:
         mock.assert_has_calls([call(*args) for args in call_args])
+
+
+def tested_module(source_module: str) -> Callable:
+    def inner(test_case: TestCase) -> TestCase:
+        test_case._source = source_module
+
+        return test_case
+    
+    return inner
