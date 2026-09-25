e2e4f76cd5aebd4717b8bb6b760a83e47bf440e2
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2026-09-25 21:59:32 +0200

    Print ytdlp options for debugging

--- src/audio_source/ytdlp_pcm_source.py
+++ src/audio_source/ytdlp_pcm_source.py
@@ -60,8 +60,8 @@ class YtdlpPCMSource(IPCMSource):
             ytdl_opts = cls._YTDL_FORMAT_OPTIONS
 
             if cookies != '':
-                logging.info(f'using cookies at {cookies}')
-                ytdl_opts['cookies'] = cookies
+                ytdl_opts['cookiefile'] = cookies
+                logging.info(ytdl_opts)
 
             ytdl = yt_dlp.YoutubeDL(ytdl_opts)
             data: dict = ytdl.extract_info(url, download=False)
