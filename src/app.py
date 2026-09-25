e146ffa5fc7119dfc9594089f605aacefc7a28ec
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2026-09-25 21:31:05 +0200

    Fix info logs not being shown if logging.error executes before logging.basicConfig

--- src/app.py
+++ src/app.py
@@ -22,7 +22,8 @@ if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s,%(levelname)s,%(message)s',
-        datefmt='%d/%m/%Y %H:%M:%S'
+        datefmt='%d/%m/%Y %H:%M:%S',
+        force=True
     )
 
     App().start()
