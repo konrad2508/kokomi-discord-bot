0666ed471f6f5f0c68f37236cfd0f8d7a6fad433
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2026-09-26 14:55:36 +0200

    Revert to using IPv4 for yt-dlp requests

--- src/audio_source/ytdlp_pcm_source.py
+++ src/audio_source/ytdlp_pcm_source.py
@@ -26,7 +26,6 @@ class YtdlpPCMSource(IPCMSource):
         'quiet': False,
         'no_warnings': True,
         'default_search': 'auto',
-        'source_address': '::',
         'extractor_args': {
             'youtube': {
                 'player_client': ['web', 'mweb', 'android']
